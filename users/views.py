ff9f9b18bbd2c31e1a1032922c334173f959ce4e
Sardorbek <nsardorbek776@gmail.com>
2025-03-13 20:48:18 +0500

    ok man

--- users/views.py
+++ users/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from users.forms import LoginForm
-
+from users.models import CustomUser
 
 # Create your views here.
 
@@ -35,37 +35,28 @@ def login_page(request):
 
     return render(request, 'users/login.html', {'form': form})
 
-
 def register_page(request):
-    form =LoginForm()
+    form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = CustomUser.objects.get(email=cd['email'])
-            if user:
-                messages.add_message(
-                    request,
-                    messages.ERROR,
-                    'Account with this creadiantials already exists'
-                )
-                return render(request, 'users/register.html')
+
+
+            if CustomUser.objects.filter(email=cd['email']).exists():
+                messages.error(request, 'Account with this email already exists.')
             else:
+                # Create user
                 user = CustomUser.objects.create_user(
                     email=cd['email'],
-                    password=cd['password'],
+                    password=cd['password']
                 )
                 user.save()
-                messages.add_message(
-                    request,
-                    messages.SUCCESS,
-                    'You have successfully registered'
-                )
-                return render(request, 'shop:index.html')
+                messages.success(request, 'You have successfully registered')
+                login(request, user)
+                return redirect('shop:index')
+
     return render(request, 'users/register.html', {'form': form})
-
-
-
 
 def logout_page(request):
     if request.method == 'POST':
